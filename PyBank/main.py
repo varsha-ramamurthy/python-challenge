5ff6e61627c20aea733e1c9c8f35e11b73d74269
varsha-ramamurthy <varsha.ramamurthy1@gmail.com>
2019-06-02 18:44:38 -0400

    PyBank code completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,17 +3,44 @@ import csv
 print(os.path.dirname(__file__))
 os.chdir(os.path.dirname(__file__))
 budget_csv = os.path.join("..\PyBank","budget_data.csv")
-#total number of months
+
 with open(budget_csv, newline="") as csvfile:
     csv_reader=csv.reader(csvfile,delimiter=",")
-    hdr  = next(csvfile)
-input_file = open("budget_data.csv","r+")
-total_months=len(list(csv.reader(input_file)))
-print(total_months)
-#net profit/loss total
-with open("budget_data.csv") as fin:
-    headerline = fin.next()
-    net_total=0
-    for row in csv.reader(fin):
+    # ignore the header
+    hdr=next(csv_reader)
+
+    total_months = 0
+    net_total = 0
+    net_average=0
+    total_net_change = 0
+    greatest_increase=0
+    greatest_decrease=0
+    profit_loss_change=0
+    greatest_increase_month=""
+    greatest_decrease_month=""
+    for row in csv_reader:
+        #increment total months, net_total 
+        total_months += 1
         net_total += int(row[1])
-    print(net_total)
+        # Computations for average change in Profit/Loss
+        if total_months != 1 :
+            profit_loss_change=int(row[1]) - prev_net
+            total_net_change += profit_loss_change
+            if(profit_loss_change>greatest_increase):
+                greatest_increase=profit_loss_change
+                greatest_increase_month=str(row[0])
+            if(profit_loss_change<greatest_decrease):
+                greatest_decrease=profit_loss_change
+                greatest_decrease_month=str(row[0])
+        prev_net = int(row[1])
+    net_average=round(total_net_change/(total_months-1),2)    
+print("Financial Analysis")
+print("-------------------------------------------")
+print(f"Total Months:" + str(total_months))
+print(f"Total:" + str(net_total))
+print(f"Average Change: $"+ str(net_average))
+print(f"Greatest Increase in Profits: $" + str(greatest_increase) + " in" + greatest_increase_month)
+print(f"Greatest Decrease in Profits: $" + str(greatest_decrease) + " in" + greatest_decrease_month)
+
+
+
